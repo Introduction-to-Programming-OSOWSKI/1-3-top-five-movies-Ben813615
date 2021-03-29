ae82f914fbad4925359e73bad9dc9a3ba1abf725
Beyer Benjamin <813615@stma.k12.mn.us>
2021-03-29 10:17:51 -0500

    topfivemovies #4

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 def topFiveMovies(a, b ,c, d, e):
     return(a, b, c, d, e)
-print topFiveMovies("Ex Machina", "Endgame", "The Thing", "The Bee Movie". "The Winter Soldier")
-=======
- def topFiveMovies(a, b, c, d, e):
-     return("Ex Machina", "Endgame", "Soul", "The Thing", "The Bee Movie")
-print topFiveMovies 
->>>>>>> d511ba0c4ada657776ddfc807f49a395a2dd4cbe
+
+print (topFiveMovies("Ex Machina", "Endgame", "The Thing", "The Bee Movie", "The Winter Soldier"))
